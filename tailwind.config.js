44ba9ed3ba0c1c839feff78304e12fe4c7b06f95
Olamide Olayemi <olamid15@gmail.com>
2025-07-15 09:28:54 +0100

    fix: update gradient order in LoginPage and enhance tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,26 @@ module.exports = {
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
+	safelist: [
+		'focus:ring-[#2f554c]',
+		'focus:border-[#457b6e]',
+		'from-[#3f9b85]',
+		'via-[#33cfbb]',
+		'to-[#1c4138]',
+		'to-[#457b6e]',
+		'from-[#457b6e]',
+		'bg-[#457b6e]',
+		'text-[#457b6e]',
+		'border-[#457b6e]',
+		'bg-[#2f554c]',
+	],
 	theme: {
 		extend: {
 			colors: {
 				'teal-custom': '#457b6e',
+				'teal-custom-1': '#3f9b85',
+				'teal-custom-2': '#34c4a2',
+				'teal-custom-3': '#2f554c',
 			},
 		},
 	},
